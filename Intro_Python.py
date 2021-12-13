d71c78975c81a95e446cb4e2806e845ad5bfd7f6
Henrique Teixeira <henriquetcro.tech@gmail.com>
2021-12-13 09:48:08 -0300

    Update the Arithmetic Operators part

--- Intro_Python.py
+++ Intro_Python.py
@@ -11,27 +11,34 @@ print(3+5)
 # "+" (Addition)
 # Example:
 3+5
+# Result: 8
 
 # "-" (Subtraction)
 # Example:
 7-2
+# Result: 5
 
 # "*" (Multiplication)
 # Example:
 7*2
+# Result: 14
 
 # "/" (Division)
 # Example:
 8/2
+# Result: 4
 
-# "/" (Module = The remainder after dividing)
+# "%" (Module = The remainder after dividing)
 # Example:
-8//2
+7%2
+# Result: 1
 
 # "**" (Exponeciation) - Important: Do not confuse with the "^", it does a different operation called "bitwise XOR"
 # Example:
 2**3
+# Result: 8
 
 # "//" (Integer Division - Divides and rounds down to the nearest integer)
-# Example: 
-2//3
+# Example:
+7//3
+# Result: 2
