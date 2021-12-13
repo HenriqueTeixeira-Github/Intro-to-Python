354c559e06ed200b047166a57d9a4a70336bbdf2
Henrique Teixeira <henriquetcro.tech@gmail.com>
2021-12-13 09:46:00 -0300

    Added the Arithmetic Operators

--- Intro_Python.py
+++ Intro_Python.py
@@ -5,3 +5,33 @@
 
 # Example:
 print(3+5)
+
+# Arithmetic Operators
+
+# "+" (Addition)
+# Example:
+3+5
+
+# "-" (Subtraction)
+# Example:
+7-2
+
+# "*" (Multiplication)
+# Example:
+7*2
+
+# "/" (Division)
+# Example:
+8/2
+
+# "/" (Module = The remainder after dividing)
+# Example:
+8//2
+
+# "**" (Exponeciation) - Important: Do not confuse with the "^", it does a different operation called "bitwise XOR"
+# Example:
+2**3
+
+# "//" (Integer Division - Divides and rounds down to the nearest integer)
+# Example: 
+2//3
